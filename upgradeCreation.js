7076d471914728777a2f713b5dfbb4be4695374f
hello6798Schoolacc <brianskiovox@gmail.com>
2024-12-12 15:51:23 -0700

    Update upgradeCreation.js

--- upgradeCreation.js
+++ upgradeCreation.js
@@ -26,3 +26,23 @@ function upgradeMissing(n) {
     let a=gameData.upgrades?.["up"+n]
     return a===undefined;
 }
+function setupUpgrades(infReset=false) {
+    let upgradeCount=11;
+    if(infReset) {
+        upgradeIndex=1;
+        for(let i=1; i<=upgradeCount; i++) {
+            removeEvent("#up"+i);
+        }
+    }
+    createUpgrade(10, 1, 1, 1, [1.5,10,1], !upgradeMissing(1));
+    createUpgrade(25, 1, 1, 1, [1.35,25,1], !upgradeMissing(2));
+    createUpgrade(200, 2, 1.5, 2, [200,2.3,2.5], !upgradeMissing(3));
+    createUpgrade(500, 2, 1.5, 2, [500,3.3,3], !upgradeMissing(4));
+    createUpgrade(50000, 3, 0.1, 3, [1.17,50000,3], !upgradeMissing(5));
+    createUpgrade(1e17, 4, 0.1, 3, [1.5,1e17,4], !upgradeMissing(6));
+    createUpgrade(1e25, 4, 0.1, 3, [3,1e25,5], !upgradeMissing(7));
+    createUpgrade(1e35, 4, 0.1, 3, [5,1e35,6], !upgradeMissing(8));
+    createUpgrade(1e66, 4, 0.1, 3, [7.5,1e66,7], !upgradeMissing(9));
+    createUpgrade(1e100, 4, 0.15, 3, [1000,1e100,8], !upgradeMissing(10));
+    createUpgrade(1e165, 4, 0.001, 1, [1,1e165,1], !upgradeMissing(11));
+}
